8ca618beb2a44b2e6db8348255ff7607f229d91f
PriyanshiKantariya <priyanshikantariya98@gmail.com>
2025-03-21 19:08:22 +0530

    Priyanshi : Minor changes in the sidebar

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -88,25 +88,9 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         url: "#",
         icon: Settings2,
       },
-    ],
-    projects: [
-      {
-        name: "Design Engineering",
-        url: "#",
-        icon: Frame,
-      },
-      {
-        name: "Sales & Marketing",
-        url: "#",
-        icon: PieChart,
-      },
-      {
-        name: "Travel",
-        url: "#",
-        icon: Map,
-      },
-    ],
-  }
+    ]
+  
+}
 
   return (
     <Sidebar collapsible="icon" {...props}>
@@ -115,7 +99,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+       
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user}  />
